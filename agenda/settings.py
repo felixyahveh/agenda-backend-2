26de041673b8e6ad0f5a4c18fdb34b9d0ca99cc4
felixyahveh <72472596+felixyahveh@users.noreply.github.com>
2022-11-22 17:08:21 -0600

    Update settings.py

--- agenda/settings.py
+++ agenda/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7@ixg(%jwbpgt3+_b9!rjj*v$kli+tu328sok%j6m$p^qa_^5_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
